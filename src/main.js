badcefda594ef0bed78f5217568dcdde315dd48e
Milen <millalen.pg@gmail.com>
2024-03-28 11:43:11 -0300

    filterBy development

--- src/main.js
+++ src/main.js
@@ -11,10 +11,7 @@ renderItems(data);
 
 nenSelect.addEventListener('change', (e)=> {
   const filterByNen = filterData(data, 'nenType', e.target.value)
-  const ul = document.querySelectorAll('ul') // DUDA: por qué cuando lo intenté con getElementById no funcionó, pero con querySelectorAll sí???
-  ul.forEach((card)=> { //BUSCAR COMO USAR EL remove() DIRECTO EN EL UL SIN BUCLE
-    card.remove()
-  }); // ...Limpiar el contenedor antes de renderizar el resultado del filtro. 
+  searchRoot.innerHTML = '';
   searchRoot.appendChild(renderItems(filterByNen)) //AGREGA las cards que cumplen con el filtro, mas se sigue renderizando el resto. 
 })
 
@@ -22,9 +19,6 @@ nenSelect.addEventListener('change', (e)=> {
 cleanerButton.addEventListener('click', ()=> {
   //nenSelect.value===''  QUE LOS SELECT SE REESTABLEZCAN A SU DEFAULT
   //document.getElementById('filter-nen').reset(); NO FUNCIONA
-  const ul = document.querySelectorAll('ul') //DUDA: por q cuando saco esta variable del scope, deja de funcionar?
-  ul.forEach((card)=> {
-    card.remove()
-  });
+  searchRoot.innerHTML = "";
   searchRoot.appendChild(renderItems(data));
 })
